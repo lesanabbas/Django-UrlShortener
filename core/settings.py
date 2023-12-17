b36df564ed9108893153f798b8d9ef58c51377d9
Lesan Abbas <63160640+lesanabbas@users.noreply.github.com>
2023-12-17 21:53:21 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", "*"]
 
 
 # Application definition
